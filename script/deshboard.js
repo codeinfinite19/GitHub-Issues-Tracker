965377ebe37b3067623df625471912ba9a26d982
codeinfinite19 <shahedhassan572@gmail.com>
2026-03-09 12:51:53 +0600

    set the labels bg

--- script/deshboard.js
+++ script/deshboard.js
@@ -1,10 +1,16 @@
 
 let allIssues = [];
+
 const loadPost = async()=>{
     const url = "https://phi-lab-server.vercel.app/api/v1/lab/issues";
     const res = await fetch(url);
     const data = await res.json();
+
+    allIssues = data.data;
+
     displayData(data.data);
+
+    updateCounts();
 }
 
 const displayData = (info)=>{
@@ -12,16 +18,31 @@ const displayData = (info)=>{
     cardDisplay.innerHTML = "";
 
 
-
-    
-  
-
     info.forEach((issue)=>{
          
 
     const borderTopcolor = issue.status === "open"
     ? "border-t-4 border-[#00A96E]"
     : "border-t-4 border-[#A855F7]";
+
+    const priorityStatus = {
+        high: "bg-[#FEECEC]",
+        medium: "bg-[#FFF6D1]",
+        low: "bg-[#9CA3AF]"
+    };
+    const priorityStyle = priorityStatus[issue.priority.toLowerCase()];   
+    
+    const labelStyles = {
+        bug: "badge badge-outline border-red-400 text-red-500",
+        "help wanted": "badge badge-outline border-yellow-400 text-yellow-600",
+        documentation:"badge badge-outline border-yellow-400 text-yellow-600",
+       //good first issue : "badge badge-outline border-green-400 text-green-600"
+        enhancement: "badge badge-outline border-green-400 text-green-600"
+        };
+
+    const labelshtml = issue.labels
+        .map(label => `<span class="${labelStyles[label] || "badge"}">${label.toUpperCase()}</span>`)
+        .join("");
 
     // const op=0;
     // const cl =0;
@@ -43,16 +64,15 @@ const displayData = (info)=>{
 
                     <div class="flex justify-between px-4">
                         <img class= "w-[20px]" src="./assets/Open-Status.png" alt="">
-                        <button class="btn btn-soft btn-secondary" >${issue.priority}</button>
+                        <button class="btn ${priorityStyle}" >${issue.priority}</button>
                     </div>
                     
-                    <h1 class="font-bold mt-2 mb-2"> ${issue.title} </h1>
+                    <h1 class="font-bold mt-2 mb-2 line-clamp-2"> ${issue.title} </h1>
                     <p class="text-[#64748B]"> ${issue.description} </p>
 
                     <div class="flex gap-2 py-2">
-                        <button class="btn btn-primary">BUG</button>
-                        <button class="btn btn-soft btn-secondary">HELP WANTED</Button>
-                        <button class="btn btn-soft btn-secondary">Enhancement</Button>
+                   
+                  ${labelshtml}
 
                     </div>
                     <div class="h-[1px] w-[100%] bg-black"></div>
@@ -73,4 +93,94 @@ const displayData = (info)=>{
    
 }
 
+
+// Total number jobs : open jobs and closed jobs count
+
+const updateCounts = (issues)=>{
+    const total = allIssues.length;
+    const open = allIssues.filter(issue => issue.status === "open").length;
+    const closed = allIssues.filter(issue => issue.status === "closed").length;
+
+    //update the count 
+
+    document.getElementById("total-count").innerText = total;
+    document.getElementById("open-count").innerText = open;
+    document.getElementById("closed-count").innerText = closed;
+
+}
+
+// chnae the color of the active button:::
+
+const setActiveButton = (id)=>{
+    document.getElementById("btn-all").classList.remove("btn-primary");
+    document.getElementById("btn-open").classList.remove("btn-primary");
+    document.getElementById("btn-closed").classList.remove("btn-primary");
+
+    document.getElementById(id).classList.add("btn-primary");
+
+};
+
+// filter button
+
+const showAll = ()=>{
+    displayData(allIssues);
+    updateCounts(allIssues);
+    setActiveButton("btn-all");
+    showCounts("all");
+};
+
+const showOpen = () => {
+    const openIssues = allIssues.filter(issue => issue.status=== "open");
+    displayData(openIssues);
+    updateCounts(openIssues);
+    setActiveButton("btn-open");
+     showCounts("open");
+};
+
+const showClosed = ()=>{
+    const closedIssues = allIssues.filter(issue => issue.status==="closed");
+    displayData(closedIssues);
+    setActiveButton("btn-closed")
+    updateCounts(closedIssues);
+     showCounts("closed");
+};
+
+
+
+
+const showCounts = (type) =>{
+    document.getElementById("total-box").classList.add("hidden");
+    document.getElementById("open-box").classList.add("hidden");
+    document.getElementById("closed-box").classList.add("hidden");
+
+    document.getElementById("allStatusIcon").classList.add("hidden");
+    document.getElementById("openStatusIcon").classList.add("hidden");
+    document.getElementById("closedStatusIcon").classList.add("hidden");
+    
+    
+    
+
+    if(type === "all"){
+      document.getElementById("total-box").classList.remove("hidden");
+       document.getElementById("allStatusIcon").classList.remove("hidden");
+
+    }
+    if(type==="open"){
+        document.getElementById("open-box").classList.remove("hidden");
+        document.getElementById("openStatusIcon").classList.remove("hidden");
+    }
+
+    if(type === "closed"){
+        document.getElementById("closed-box").classList.remove("hidden");
+        document.getElementById("closedStatusIcon").classList.remove("hidden");
+    
+    }
+
+};
+
+
+
+
 loadPost();
+showCounts("all");
+setActiveButton("btn-all");
